9a52ac56451723fea080d310087d893bd06d6a32
lmj3498 <rhtnvlfem@naver.com>
2017-11-15 14:04:30 +0900

    3. 세 번째 커밋

--- first/src/main/java/first/sample/controller/SampleController.java
+++ first/src/main/java/first/sample/controller/SampleController.java
@@ -1,7 +1,9 @@
 package first.sample.controller;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.annotation.Resource;
 
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import first.common.common.CommandMap;
 import first.sample.service.SampleService;
 
 @Controller
@@ -19,16 +22,33 @@ public class SampleController {
     @Resource(name="sampleService")
     private SampleService sampleService;
 
-	
+	//인터셉터 및 기본 컨트롤러
 	@RequestMapping(value="/sample/openSampleList.do")
 	public ModelAndView openSampleList(Map<String,Object> commandMap) throws Exception{
         ModelAndView mv = new ModelAndView("/sample/boardList");
         //log.debug("인터셉터 테스트");
-        
+        //System.out.println(commandMap);
         List<Map<String, Object>> list = sampleService.selectBoardList(commandMap);
         mv.addObject("list", list);
-        
+        //System.out.println(list);
         return mv;
     }
+	
+	//CustomMapArgumentResolver 테스트
+	//MapArguemtResolver를 등록하지 않았다면, 컨트롤러에서 request.getParameter 메서드 등을 이용하여 하나씩 가져와야 함
+	@RequestMapping(value="/sample/testMapArgumentResolver.do")
+	public ModelAndView testMapArgumentResolver(CommandMap commandMap) throws Exception{
+	    ModelAndView mv = new ModelAndView("");
+	     
+	    if(commandMap.isEmpty() == false){
+	        Iterator<Entry<String,Object>> iterator = commandMap.getMap().entrySet().iterator();
+	        Entry<String,Object> entry = null;
+	        while(iterator.hasNext()){
+	            entry = iterator.next();
+	            log.debug("key : "+entry.getKey()+", value : "+entry.getValue());
+	        }
+	    }
+	    return mv;
+	}
 
 }
